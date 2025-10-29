8de13a67ad08672e3042916cbf81e83c83b78e17
Winston Baker <winstonthebaker@gmail.com>
2025-10-28 22:18:12 -0700

    added makefile

---
+++ build/CMakeFiles/InventoryManagement.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/winstonthebaker/Documents/School/Cpts 223/pa3-inventory-management-winstonthebaker/main.cpp" "CMakeFiles/InventoryManagement.dir/main.cpp.o" "gcc" "CMakeFiles/InventoryManagement.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/InventoryManagement.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/InventoryManagement.dir/main.cpp.o"
+  "CMakeFiles/InventoryManagement.dir/main.cpp.o.d"
+  "InventoryManagement"
+  "InventoryManagement.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/InventoryManagement.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
